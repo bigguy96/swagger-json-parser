e67445f0b9f8b839d426be4077918661aab20183
GT <gaetan23@msn.com>
2020-07-01 15:36:19 -0400

    display some infomation.

---
+++ JsonParser/Class1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonParser.Classes
+{
+    public class Rootobject
+    {
+        public Path[] paths { get; set; }
+    }
+
+    public class Path
+    {
+        public string enpoint { get; set; }
+        public Post post { get; set; }
+        public Get get { get; set; }
+        public Put put { get; set; }
+        public Delete delete { get; set; }
+        public Head head { get; set; }
+    }
+
+    public class Post
+    {
+        public string[] tags { get; set; }
+        public string summary { get; set; }
+        public string description { get; set; }
+        public string operationId { get; set; }
+        public string[] consumes { get; set; }
+        public string[] produces { get; set; }
+        public Parameter[] parameters { get; set; }
+        public Responses responses { get; set; }
+    }
+
+    public class Responses
+    {
+        public R200 r200 { get; set; }
+    }
+
+    public class R200
+    {
+        public string description { get; set; }
+        public Schema schema { get; set; }
+    }
+
+    public class Schema
+    {
+        public string type { get; set; }
+    }
+
+    public class Parameter
+    {
+        public string name { get; set; }
+        public string @in { get; set; }
+        public string description { get; set; }
+        public bool required { get; set; }
+        public Schema1 schema { get; set; }
+        public string type { get; set; }
+        public string format { get; set; }
+        public string[] renum { get; set; }
+    }
+
+    public class Schema1
+    {
+        public string _ref { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Get
+    {
+        public string[] tags { get; set; }
+        public string operationId { get; set; }
+        public object[] consumes { get; set; }
+        public string[] produces { get; set; }
+        public Parameter1[] parameters { get; set; }
+        public Responses1 responses { get; set; }
+        public string summary { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Responses1
+    {
+        public R2001 r200 { get; set; }
+    }
+
+    public class R2001
+    {
+        public string description { get; set; }
+        public Schema2 schema { get; set; }
+    }
+
+    public class Schema2
+    {
+        public string type { get; set; }
+    }
+
+    public class Parameter1
+    {
+        public string name { get; set; }
+        public string @in { get; set; }
+        public bool required { get; set; }
+        public string type { get; set; }
+        public string format { get; set; }
+        public string description { get; set; }
+        public string[] renum { get; set; }
+    }
+
+    public class Put
+    {
+        public string[] tags { get; set; }
+        public string summary { get; set; }
+        public string operationId { get; set; }
+        public string[] consumes { get; set; }
+        public string[] produces { get; set; }
+        public Parameter2[] parameters { get; set; }
+        public Responses2 responses { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Responses2
+    {
+        public R2002 r200 { get; set; }
+    }
+
+    public class R2002
+    {
+        public string description { get; set; }
+        public Schema3 schema { get; set; }
+    }
+
+    public class Schema3
+    {
+        public string type { get; set; }
+    }
+
+    public class Parameter2
+    {
+        public string name { get; set; }
+        public string @in { get; set; }
+        public string description { get; set; }
+        public bool required { get; set; }
+        public string type { get; set; }
+        public string format { get; set; }
+        public string[] renum { get; set; }
+        public Schema4 schema { get; set; }
+    }
+
+    public class Schema4
+    {
+        public string type { get; set; }
+        public string format { get; set; }
+        public string _ref { get; set; }
+    }
+
+    public class Delete
+    {
+        public string[] tags { get; set; }
+        public string summary { get; set; }
+        public string description { get; set; }
+        public string operationId { get; set; }
+        public object[] consumes { get; set; }
+        public string[] produces { get; set; }
+        public Parameter3[] parameters { get; set; }
+        public Responses3 responses { get; set; }
+    }
+
+    public class Responses3
+    {
+        public R2003 r200 { get; set; }
+    }
+
+    public class R2003
+    {
+        public string description { get; set; }
+        public Schema5 schema { get; set; }
+    }
+
+    public class Schema5
+    {
+        public string type { get; set; }
+    }
+
+    public class Parameter3
+    {
+        public string name { get; set; }
+        public string @in { get; set; }
+        public string description { get; set; }
+        public bool required { get; set; }
+        public string type { get; set; }
+        public string format { get; set; }
+        public string[] renum { get; set; }
+    }
+
+    public class Head
+    {
+        public string[] tags { get; set; }
+        public string summary { get; set; }
+        public string description { get; set; }
+        public string operationId { get; set; }
+        public object[] consumes { get; set; }
+        public string[] produces { get; set; }
+        public Parameter4[] parameters { get; set; }
+        public Responses4 responses { get; set; }
+    }
+
+    public class Responses4
+    {
+        public R2004 r200 { get; set; }
+    }
+
+    public class R2004
+    {
+        public string description { get; set; }
+        public Schema6 schema { get; set; }
+    }
+
+    public class Schema6
+    {
+        public string type { get; set; }
+    }
+
+    public class Parameter4
+    {
+        public string name { get; set; }
+        public string @in { get; set; }
+        public string description { get; set; }
+        public bool required { get; set; }
+        public string type { get; set; }
+        public string format { get; set; }
+        public string[] renum { get; set; }
+    }
+
+}
---
+++ JsonParser/Program.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace JsonParser
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            // Read the file and display it line by line.  
+            var counter = 0;
+            var line = string.Empty;
+            var lines = new List<string>();
+            var sb = new System.Text.StringBuilder("");
+            var mydocs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var json = Path.Combine(mydocs, "GCWeb", "swagger_paths.json");
+            using (var reader = new StreamReader(json))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line.Trim());
+                }
+            }
+
+            foreach (var item in lines)
+            {
+                if (counter == 6)
+                {
+                    counter = 0;
+                    continue;
+                }
+
+                if (item.Equals("}"))
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 0;
+                }
+
+                if (item.Contains("paths"))
+                {
+                    sb.Append($"{item.Replace("{", "")} [");
+                    //sb.AppendLine("{");
+                }
+                else if (item.Contains("api/v"))
+                {
+                    sb.Append("{");
+                    sb.Append(@$"""enpoint"":{item.Replace(":", "").Substring(0, item.Length - 2)},");
+                }
+                else if (counter == 5)
+                {
+                    sb.Append(item.Replace("}", "},"));
+                    counter++;
+                    //sb.AppendLine(item.Replace("}","}],")); 
+                }
+                else
+                {
+                    sb.Append(item);
+                }
+            }
+
+            sb.Remove(sb.Length - 3, 2);
+            sb.AppendLine("]}");
+            sb.Replace("200", "r200");
+            sb.Replace("$ref", "ref");
+            sb.Replace("enum", "renum");
+
+            await File.WriteAllTextAsync(Path.Combine(mydocs, "GCWeb", "swagger_test.json"), sb.ToString());
+
+            var jfile = await File.ReadAllTextAsync(Path.Combine(mydocs, "GCWeb", "swagger_test.json"));
+            //var swagger = JObject.Parse(jfile);
+            //var ds=  JsonConvert.DeserializeAnonymousType(jfile, null);
+
+            var r = JsonConvert.DeserializeObject<Classes.Rootobject>(jfile);
+
+            var post = r.paths.Where(w => w.post != null)
+                .Select(p => new
+                {
+                    p.enpoint,
+                    section = p.post.tags[0],
+                    p.post.summary,
+                    parameters = p.post.parameters.Select(pa => new { pa.name, pa.@in, pa.description, pa.renum, pa.required })
+                });
+
+            foreach (var item in post)
+            {
+                Console.WriteLine("*******************************************************");
+                Console.WriteLine($"endpoint: {item.enpoint}");
+                Console.WriteLine($"section: {item.section}");
+                Console.WriteLine($"description: {item.summary}");
+
+                foreach (var param in item.parameters)
+                {
+                    Console.WriteLine($"parameter name: {param.name}");
+                    Console.WriteLine($"parameter location: {param.@in}");
+                    Console.WriteLine($"parameter description: {param.description}");
+                    Console.WriteLine($"parameter required: {param.required}");
+                }
+
+                //foreach (var response in item.post.responses.r200.description)
+                //{
+
+                //}
+
+            }
+
+            // Suspend the screen.  
+            Console.ReadLine();
+        }
+    }
+}
